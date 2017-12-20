b326980a2c46136da5c4c19767e3a1d99a4ceb22
Luiz <luizrodrigo46@hotmail.com>
2017-12-20 20:38:54 -0300

    the ModelAdmin has been added to Channel and Category models

--- work-at-olist/channels/admin.py
+++ work-at-olist/channels/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from channels.models import Channel, Category
 
-# Register your models here.
-admin.site.register(Channel)
-admin.site.register(Category)
+
+class ChannelAdmin(admin.ModelAdmin):
+    fields = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name', 'channel', 'parent_category',)
+
+
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Category, CategoryAdmin)
