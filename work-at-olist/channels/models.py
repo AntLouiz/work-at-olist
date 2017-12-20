460b9e39eab08435330da9e9603beda65c96d9dc
Luiz <luizrodrigo46@hotmail.com>
2017-12-20 20:14:31 -0300

    slug has been added to the models

--- work-at-olist/channels/models.py
+++ work-at-olist/channels/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Channel(models.Model):
 
     name = models.CharField(max_length=50)
+    slug = models.SlugField(blank=True, null=True)
 
     class Meta:
         verbose_name = "Channel"
         verbose_name_plural = "Channels"
+
+    def save(self):
+        self.slug = slugify(self.name)
+        super(Channel, self).save()
 
     def __str__(self):
         return self.name
@@ -16,10 +22,11 @@ class Channel(models.Model):
 class Category(models.Model):
 
     name = models.CharField(max_length=50)
+    slug = models.SlugField(blank=True, null=True)
     parent_category = models.ForeignKey(
         'self',
         on_delete=models.CASCADE,
-        related_name='sub_categories',
+        related_name='categories',
         blank=True,
         null=True
     )
@@ -38,8 +45,8 @@ class Category(models.Model):
         parent_category = self.parent_category
         if parent_category:
             self.channel = parent_category.channel
-            print(self.channel)
 
+        self.slug = slugify(self.name)
         super(Category, self).save()
 
     def __str__(self):
