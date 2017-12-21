e25605d778851c9c383b1d42a7d1a40430e0f83d
Luiz <luizrodrigo46@hotmail.com>
2017-12-21 12:15:35 -0300

    the args and options has been passed to get_file_line method.

--- work-at-olist/channels/management/commands/importcategories.py
+++ work-at-olist/channels/management/commands/importcategories.py
@@ -10,7 +10,8 @@ class Command(BaseCommand):
         parser.add_argument('channel', nargs=1)
         parser.add_argument('filename', nargs=1)
 
-    def get_file_line(self, filename):
+    def get_file_line(self, *args, **options):
+        filename = options['filename'][0]
 
         if not os.path.isfile(filename):
             raise CommandError("File not found.")
@@ -24,10 +25,9 @@ class Command(BaseCommand):
         print("Trying to import the categories...")
 
         channel_desc = options['channel'][0]
-        filename = options['filename'][0]
 
         #checking if the channel inside the file is the same of the argument.
-        for line in self.get_file_line(filename):
+        for line in self.get_file_line(*args, **options):
             if line[0] != channel_desc:
                 raise Exception(
                     "The channel inside the file is different from the argument."
@@ -35,7 +35,7 @@ class Command(BaseCommand):
 
             channel_obj = Channel.objects.get_or_create(name=channel_desc)
 
-        for categories in self.get_file_line(filename):
+        for categories in self.get_file_line(*args, **options):
             channel = categories[0]
 
             #checking if the channel inside the file is the same of the argument.
