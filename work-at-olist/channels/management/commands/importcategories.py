c353d3523683b0ae63552d961e4323a131fefc9e
Luiz <luizrodrigo46@hotmail.com>
2017-12-21 11:36:06 -0300

    the handle method has been refactored with the get_file_line method.

--- work-at-olist/channels/management/commands/importcategories.py
+++ work-at-olist/channels/management/commands/importcategories.py
@@ -1,17 +1,60 @@
+import os
 from django.core.management.base import BaseCommand, CommandError
-from channels.models import Channel, Category
+from channels.models import Channel
 
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Import the channels categories from a text file.'
 
     def add_arguments(self, parser):
         parser.add_argument('channel', nargs=1)
         parser.add_argument('filename', nargs=1)
 
+    def get_file_line(self, filename):
+
+        if not os.path.isfile(filename):
+            raise CommandError("File not found.")
+
+        with open(filename, 'r') as file:
+            for line in file.readlines():
+                line = [word.strip() for word in line.split('/')]
+                yield line
+
     def handle(self, *args, **options):
-        channel = options['channel'][0]
+        print("Trying to import the categories...")
+
+        channel_desc = options['channel'][0]
         filename = options['filename'][0]
 
-        print(channel)
-        print(filename)
+        #checking if the channel inside the file is the same of the argument.
+        for line in self.get_file_line(filename):
+            if line[0] != channel_desc:
+                raise Exception(
+                    "The channel inside the file is different from the argument."
+                )
+
+            channel_obj = Channel.objects.get_or_create(name=channel_desc)
+
+        for categories in self.get_file_line(filename):
+            channel = categories[0]
+
+            #checking if the channel inside the file is the same of the argument.
+            if channel != channel_desc:
+                raise Exception(
+                    "The channel inside the file is different from the argument."
+                )
+
+            try:
+                parent_category = categories[-2] if categories[-2] != channel else None
+                category = categories[-1]
+
+                c = {
+                    'channel': channel,
+                    'parent_category': parent_category,
+                    'category': category
+                }
+
+                print(c)
+
+            except:
+                pass
