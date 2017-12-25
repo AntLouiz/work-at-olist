71bc11f1422cf4ea895668a35d105a32868b0274
Luiz <luizrodrigo46@hotmail.com>
2017-12-25 19:40:04 -0300

    a index page has been added

--- work-at-olist/workatolist/urls.py
+++ work-at-olist/workatolist/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('channels.urls')),
     url(r'^api/', include('channels.api.urls'))
 ]
